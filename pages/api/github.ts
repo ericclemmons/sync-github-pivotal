284305c8ed6ce4b4a617b0bba9d38e420b00d67b
Eric Clemmons <eric@smarterspam.com>
2020-01-29 16:54:41 -0800

    Opened issues are created in Pivotal Tracker

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -1,7 +1,15 @@
 import "dotenv-safe/config";
 import { createHmac } from "crypto";
+import got, { ResponseObject } from "got";
 import { NextApiRequest, NextApiResponse } from "next";
 import getRawBody from "raw-body";
+
+const pivotal = got.extend({
+  headers: {
+    "X-TrackerToken": process.env.PIVOTAL_TRACKER_TOKEN
+  },
+  prefixUrl: "https://www.pivotaltracker.com/services/v5"
+});
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
@@ -26,15 +34,55 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const body = JSON.parse(rawBody.toString("utf8"));
+  let response: ResponseObject = undefined;
 
-  // Echo out body until hooks are integrated
-  res.status(200).json(body);
+  // ? Should `X-GitHub-Event` (e.g. `issues`) be used here as well?
+  switch (body.action) {
+    case "opened":
+      const { issue } = body;
+
+      // https://www.pivotaltracker.com/help/api/rest/v5#projects_project_id_stories_get
+      const existing = (await pivotal(
+        `projects/${process.env.PIVOTAL_PROJECT_ID}/stories?filter=external_id:${issue.number}`
+      ).json()) as any[];
+
+      if (existing.length) {
+        return res
+          .status(409)
+          .send(
+            `GitHub issue #${issue.number} has ${existing.length} existing stories`
+          );
+      }
+
+      // https://www.pivotaltracker.com/help/api/rest/v5#projects_project_id_stories_post
+      response = await pivotal
+        .post(`projects/${process.env.PIVOTAL_PROJECT_ID}/stories`, {
+          json: {
+            // external_id: "issue.number",
+            created_at: issue.created_at,
+            description: issue.body,
+            name: issue.title
+          }
+        })
+        .json();
+
+      break;
+
+    default:
+      return res
+        .status(501)
+        .json({ message: `${JSON.stringify(body.action)} is not implemented` });
+  }
+
+  res.status(200).json(response ?? { message: "Success" });
 };
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   handle(req, res).catch(error => {
-    console.error(error);
-    res.status(500).json({ message: error.message });
+    console.error(error.response);
+    res
+      .status(error.response?.statusCode ?? 500)
+      .json(error.response?.body ?? { message: error.message });
   });
 };
 
