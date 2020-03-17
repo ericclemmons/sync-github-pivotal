0da4401dd1af1b692c047247f5740f5fae0a59d2
Eric Clemmons <eric@smarterspam.com>
2020-03-17 10:34:15 -0700

    Move error handler to api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,29 @@
-import express from "express";
+import { isBoom, boomify, Boom } from "@hapi/boom";
+import express, { NextFunction, Request, Response } from "express";
 
 import { middleware as github } from "./github";
 import { middleware as pivotal } from "./pivotal";
 
 export const api = express()
   .use("/github", github)
-  .use("/pivotal", pivotal);
+  .use("/pivotal", pivotal)
+  .use(
+    (error: Boom | Error, req: Request, res: Response, next: NextFunction) => {
+      let data;
+
+      try {
+        // @ts-ignore
+        // https://github.com/sindresorhus/got#usage
+        data = error.response?.body ?? "null";
+        data = JSON.parse(data);
+      } catch (error) {}
+
+      const boom = isBoom(error) ? (error as Boom) : boomify(error, { data });
+
+      if (boom.isServer) {
+        console.error(boom);
+      }
+
+      return res.status(boom.output.statusCode).json(boom.output.payload);
+    }
+  );
